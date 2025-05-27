fe45d7b93ea68fd284c35eade3d65d26242da73f
DuyMys <duymy0101@gmail.com>
2025-05-27 16:42:10 +0700

    duymy add

---
+++ src/instruction/BFormatInstruction.java
@@ -0,0 +1,46 @@
+package instruction;
+
+import java.util.BitSet;
+
+/**
+ * BFormatInstruction is a class that represents a branch format instruction in the LEGv8 architecture.
+ * It extends the Instruction class and provides methods to disassemble the instruction and extract its components.
+ */
+public class BFormatInstruction extends Instruction {
+    private final int offset;
+
+    // --- Constructor ---
+    /**
+     * Constructor for BFormatInstruction.
+     * @param bytecode The bytecode of the instruction as a BitSet.
+     * @param definition The InstructionDefinition for this instruction.
+     */
+    public BFormatInstruction(BitSet bytecode, InstructionDefinition definition) {
+        super(bytecode, definition);
+        this.offset = getAddress_B();
+    }
+
+    // --- Instruction Methods ---
+    /**
+     * @return The instruction as assembled string.
+     *         The string is formatted as "mnemonic #offset" (offset in instructions).
+     */
+    @Override
+    public String disassemble() {
+        String mnemonic = getDefinition().getMnemonic();
+        // Offset in instructions
+        int displayOffset = (offset & 0x2000000) != 0 ? (offset | 0xFC000000) : offset;
+        return String.format("%-6s #%d", mnemonic, displayOffset);
+    }
+
+    // --- Getters ---
+    @Override
+    public int getAddress_B() {
+        return extractBits(bytecode, 0, 25); // Bits 0-25
+    }
+
+    @Override
+    public int getImmediate_I() {
+        throw new UnsupportedOperationException("getImmediate_I not supported for B format");
+    }
+}
